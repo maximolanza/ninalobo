a035b96ba3fede036addda8c9fefb2a02c2a9750
Máximo Lanza <max.slanza@gmail.com>
2021-11-05 03:12:10 -0300

    Unknown property 'allowfullscreen' deleted
    Unknown property 'allowfullscreen' deleted

--- components/home/SpotifyDisc.js
+++ components/home/SpotifyDisc.js
@@ -8,7 +8,6 @@ const SpotifyDisc = ({ code, key }) => {
                 width="100%"
                 height="380"
                 frameBorder="0"
-                allowfullscreen=""
                 allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
 
         </div>
